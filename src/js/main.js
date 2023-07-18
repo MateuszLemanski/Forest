d7baefb5eb0d7f169d2fc7e342316a66eb2814a5
Mateusz Lemański <mat.lemanski@gmail.com>
2023-07-18 12:01:35 +0200

    wrote script for clear button to clear forms

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,19 @@
 const footerYear = document.querySelector('.present-year');
-
+const clearBtn = document.querySelector('.clear-btn');
+const firstName = document.querySelector('#name');
+const surname = document.querySelector('#surname');
+const email = document.querySelector('#email');
+const msg = document.querySelector('#msg');
+const formFields = [firstName, surname, email, msg]
 
 const handleNewYear = () => {
     const year = new Date().getFullYear();
     footerYear.innerText = year;
 }
 
+const clearForm = () => {
+    formFields.forEach(element => element.value = '')
+}
 
 handleNewYear();
+clearBtn.addEventListener('click', clearForm);
