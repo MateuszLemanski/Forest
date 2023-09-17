0818025e8bb6a978f46cb9a64d91c89e5cf3ee15
Mateusz Lemański <mat.lemanski@gmail.com>
2023-09-17 09:44:04 +0200

    wrote scrollspy script, still wroking on it, and create .active

--- src/js/main.js
+++ src/js/main.js
@@ -6,6 +6,30 @@ const email = document.querySelector('#email');
 const msg = document.querySelector('#msg');
 const formFields = [firstName, surname, email, msg];
 
+let section = document.querySelectorAll('section')
+let navLinks = document.querySelectorAll('.nav__ul a')
+
+// const nav = document.querySelector('.nav__ul');
+
+window.onscroll = () => {
+    section.forEach(sec => {
+        let top = window.scrollY
+        let offset = sec.offsetTop
+        let height = sec.offsetHeight
+        let id = sec.getAttribute('id')
+
+        if(top >= offset && top < offset + height){
+            navLinks.forEach(links => {
+                links.classList.remove('active')
+                document.querySelector('.nav__ul a[href*=' + id + ']').classList.add('active')
+            })
+        }
+    })
+}
+
+
+
+
 const handleNewYear = () => {
     const year = new Date().getFullYear();
     footerYear.innerText = year;
